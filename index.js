a6406606064c9d316bf9693ab35e9a88e0b61462
Kunal_Pawar_GIT <pawarkp21.elec@coep.ac.in>
2024-05-24 15:58:06 +0530

    handled DELETE requests

--- index.js
+++ index.js
@@ -75,8 +75,35 @@ app.patch('/jokes/:id', (req,res)=>{
 })
 
 //7. DELETE Specific joke
+app.delete("/jokes/:id", (req, res)=>{
+  const id = parseInt(req.params.id);
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  if(searchIndex > -1){
+    jokes.splice(searchIndex, 1);
+    res.sendStatus(200);
+  } else{
+    res
+    .status(404)
+    .json({
+      error: `Joke with id: ${id} not found.`
+    });  
+  }
+  
+});
 
 //8. DELETE All jokes
+app.delete('/all', (req, res)=>{
+  const val = req.query.key;
+  console.log(val);
+  if(val === masterKey){
+    jokes = [];
+    res.sendStatus(200);
+  } else {
+    res
+    .status(404)
+    .send("Master key Invalid.");
+  }
+})
 
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
